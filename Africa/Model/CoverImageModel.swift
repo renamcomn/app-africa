b4025e8286ec540d81b2d0ab951989ff8ff79eb6
Renan Henrique <renamcomn@Renans-Mac-mini.local>
2024-03-12 23:17:43 -0300

    create AnimalModel, AnimalView

---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Renan Henrique on 12/03/24.
+//
+
+import SwiftUI
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
